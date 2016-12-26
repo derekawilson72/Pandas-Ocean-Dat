48d530978320129fc9b3460b42d3b3a2a701a885
Derek Wilson <derekawilson72@gmail.com>
2016-12-26 11:40:36 -0500

    Add files via upload

--- oceanData.py
+++ oceanData.py
@@ -1,61 +1,119 @@
+
+
+
 import pandas as pd
 import datetime
-import numpy as  np
+import numpy as np
 from matplotlib import pyplot as plt
+from matplotlib.pyplot import  Line2D 
+
+class oceanDataPlot():
+    """
+    merge two datasets with intersecting (overlapping) date ranges.
+    Plot the data as a single dataset and export as a netcdf.
+    """
+    
+    fig=None
+    axs=None
+
+    WaveData=None
+    WindData=None
+    rng1=None
+    rng2=None
+    exRng1=None
+
+    def __init__(self):
+
+        self.setSampleData()
 
 
+    def setSampleData(self):
+        """set two arbitrary date ranges with overlapping dates"""
 
-rng  = pd.date_range('1/1/2011', periods=72, freq='H')
-rng1 = pd.date_range(start='1/1/2011', end='1/10/2011', freq='360S')
-rng2 = pd.date_range(start='1/2/2011', end='1/08/2011', freq='360S')
+        rng1 = pd.date_range(start='1/1/2011', end='1/10/2011', freq='360S')
+        rng2 = pd.date_range(start='1/2/2011', end='1/08/2011', freq='360S')
 
-WaveData=pd.DataFrame(rng1,columns=['date'])
-WaveData['Wvht']=np.random.randn(len(WaveData), 1)*10
-WaveData['Wvdr']=np.random.randint(0,360,size=(len(WaveData),1))
-WaveData=WaveData.set_index('date')
+        WaveData=pd.DataFrame(rng1,columns=['date'])  ##initialize wave dataset
+        WaveData['Wvht']=np.random.randn(len(WaveData), 1)*10  ##append values for column data
+        WaveData['Wvdr']=np.random.randint(0,360,size=(len(WaveData),1))
+        WaveData=WaveData.set_index('date')
+        self.rng1=rng1
+        self.WaveData=WaveData
+        
+        WindData=pd.DataFrame(rng2,columns=['date'])  ##do the same for wind data 
+        WindData['Wspd']=np.random.rand(len(WindData), 1)*5
+        WindData['Wndr']=np.random.randint(0,360,size=(len(WindData),1))
+        WindData=WindData.set_index('date')
+        self.rng2=rng2
+        self.WindData=WindData
+        
 
-WindData=pd.DataFrame(rng2,columns=['date'])
-WindData['Wspd']=np.random.rand(len(WindData), 1)*5
-WindData['Wndr']=np.random.randint(0,360,size=(len(WindData),1))
-WindData=WindData.set_index('date')
+    def intersectDates(self):
+        """
+        determine common date range between two datasets
+        """
+
+        idx1 = pd.Index(self.rng1)
+        idx2 = pd.Index(self.rng2)
+        cdx=idx1.intersection(idx2) #set to intersection
+        ##set two DataFrames to commonData
+        commonData=self.WaveData.loc[cdx]
+        commonData[self.WindData.columns]=self.WindData.loc[cdx]
+
+        #extreme wind and wave Data
+        #commonData[commonData['Wvht']>2] ##wave height above 2m
+        #commonData[commonData['Wspd']>4] ##wave height above 4m/s
+
+        self.exRng1=((commonData['Wvht']>2) & (commonData['Wspd']>4))  ##determine intersecting extreme range
+
+    def plotDates(self):
+        """
+        plotting datasets of wind and wave data as well as the extreme events
+        """
+
+        
+        
+        ##determine common date range between two datasets
+        idx1 = pd.Index(self.rng1)
+        idx2 = pd.Index(self.rng2)
+        cdx=idx1.intersection(idx2) #set to intersection
+        ##set two DataFrames to commonData
+        commonData=self.WaveData.loc[cdx]
+        commonData[self.WindData.columns]=self.WindData.loc[cdx]
+
+        #extreme wind and wave Data
+        #commonData[commonData['Wvht']>2] ##wave height above 2m
+        #commonData[commonData['Wspd']>4] ##wave height above 4m/s
+
+        exRng1=self.exRng1
+        
+        
+        fig = plt.figure(figsize=(12,4))
+        ax1 = fig.add_subplot(211)
+        
+        l1=commonData['Wvht'].plot(ax=ax1,title='WaveHeight' )  ##create timeseries of wave data
+        ax1.set_ylabel('WvHt [m]')
 
 
+        ax2 = fig.add_subplot(212)
+        l2=commonData['Wspd'].plot(ax=ax2,title='WindSpeed',color='green')
+        ##change color anytime
+        l2.lines[0].set_color('green')
+        ax2.set_ylabel('Wspd [m/s]')
+        ax2.set_xlabel('date')
+        fig.subplots_adjust(left=0.05, right=0.95, top=0.95, bottom=0.15)
 
-##determine common date range
-idx1 = pd.Index(rng1)
-idx2 = pd.Index(rng2)
-cdx=idx1.intersection(idx2)  #set to intersection
+        redWave=Line2D(commonData[exRng1].index , commonData[exRng1]['Wvht'] , linestyle='',
+                       color='r', marker='o', markersize=5, fillstyle='full')
+        ax1.add_line(redWave)
 
+        redWind=Line2D(commonData[exRng1].index , commonData[exRng1]['Wspd'] , linestyle='',
+                       color='r', marker='o', markersize=5, fillstyle='full')
+        ax2.add_line(redWind)
 
-##set two DataFrames to commonData
-commonData=WaveData.loc[cdx]
-commonData[WindData.columns]=WindData.loc[cdx]
+        self.fig=fig
+        self.axs=[ax1,ax2]
+        
+        fig.canvas.draw()
+        fig.savefig('new.png')
 
-
-
-##extreme wind and wave Data
-commonData[commonData['Wvht']>2]  ##wave height above 2m
-commonData[commonData['Wspd']>4]  ##wave height above 4m/s
-
-
-
-
-
-fig = plt.figure(figsize=(12,4))
-ax = fig.add_subplot(211)
-l1=commonData['Wvht'].plot(ax=ax,title='WaveHeight' )
-ax.set_ylabel('WvHt [m]')
-ax.set_xlabel('date')
-
-ax = fig.add_subplot(212)
-l2=commonData['Wspd'].plot(ax=ax,title='WindSpeed',color='green')
-##change color anytime
-l2.lines[0].set_color('green')
-
-ax.set_ylabel('WvHt [m]')
-ax.set_xlabel('date')
-
-fig.subplots_adjust(left=0.05, right=0.95, top=0.95, bottom=0.15)
-fig.canvas.draw()
-fig.savefig('new.png')
-ax.set_xlabel('date')
